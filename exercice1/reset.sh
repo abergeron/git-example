560fd991e3f614e65400cdeec0152b33071c1ab5
Arnaud Bergeron <abergeron@gmail.com>
2019-12-04 14:46:40 -0500

    Add a reset script

---
+++ exercice1/reset.sh
@@ -0,0 +1,5 @@
+git checkout master
+git reset master --hard origin/master
+git branch -f add_cherry 5324f5712a2fa8b6c8b71bf9c4c356623001fb79
+git branch -f add_lemons 151a60b0d8d5cec8e4d3207b84f732412ab5ff48
+git branch -f vegetable_fruits 40d23b330b7dee9af7a9a4b9e1f7331db50a78fb
